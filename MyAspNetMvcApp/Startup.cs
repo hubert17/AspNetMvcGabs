c6775ce82e7b5219572b5df6f0f1cca14b776095
hubert17 <hewbertgabon@gmail.com>
2017-07-20 00:39:26 +0800

    Jul 20 commits

--- MyAspNetMvcApp/Startup.cs
+++ MyAspNetMvcApp/Startup.cs
@@ -7,7 +7,7 @@ namespace MyAspNetMvcApp
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
-        {
+        {            
             ConfigureAuth(app);
         }
     }
