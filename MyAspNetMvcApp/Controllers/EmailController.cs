fa416292711012342899545261a807f380d80216
Bernard Gabon <hewbertgabon@gmail.com>
2017-09-03 16:15:15 +0800

    Added Sending Email Helper

---
+++ MyAspNetMvcApp/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyAspNetMvcApp.Models;
+
+namespace MyAspNetMvcApp.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(Email mail, HttpPostedFileBase Attachment)
+        {
+            var attachment = Gabs.Helpers.EmailUtil.FileToAttachment(Attachment);
+            TempData["msg"] = Gabs.Helpers.EmailUtil.SendEmail(mail.MailTo, mail.Subject, mail.Message, attachment);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyAspNetMvcApp/Helpers/EmailUtil.cs
+++ MyAspNetMvcApp/Helpers/EmailUtil.cs
@@ -14,25 +14,31 @@ namespace Gabs.Helpers
 {
     public class EmailUtil
     {
-        private const string ACCOUNT_EMAIL = "hewbertgabon@gmail.com";
-        private const string ACCOUNT_PASSWORD = "mypassword";
+        private const string ACCOUNT_EMAIL = "yuberto.gabon@gmail.com";
+        private const string ACCOUNT_PASSWORD = "my_password";
         private const string SMTP_HOST = "smtp.gmail.com"; // smtp-mail.outlook.com, smtp.mail.yahoo.com
         private const int SMTP_PORT = 587;
+        private const bool REQUIRE_SSL = true;
         private const bool IS_HTML = true;
 
-        public bool SendEmail(string mailTo, string subject, string body, string attachmentFile = "", EmailAttachment attachment = null, string mailCc = "", string mailBc = "", string mailReplyTo = "")
+        public static bool SendEmail(IEnumerable<string> mailTos, string subject, string body, EmailAttachment attachment = null, string attachmentFile = "", string mailCc = "", string mailBc = "", string mailReplyTo = "")
         {
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = SMTP_HOST;
-            smtp.Port = SMTP_PORT;
-            smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential(ACCOUNT_EMAIL, ACCOUNT_PASSWORD);
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtp.UseDefaultCredentials = false;
+            SmtpClient smtp = new SmtpClient()
+            {
+                Host = SMTP_HOST,
+                Port = SMTP_PORT,
+                EnableSsl = REQUIRE_SSL,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new System.Net.NetworkCredential(ACCOUNT_EMAIL, ACCOUNT_PASSWORD)
+            };
 
             MailMessage mail = new MailMessage();
             mail.From = new MailAddress(ACCOUNT_EMAIL);
-            mail.To.Add(mailTo);
+            foreach(var mailTo in mailTos)
+            {
+                mail.To.Add(mailTo);
+            }
             if (!String.IsNullOrEmpty(mailCc)) mail.CC.Add(mailCc);
             if (!String.IsNullOrEmpty(mailBc)) mail.Bcc.Add(mailBc);
             if (!String.IsNullOrEmpty(mailReplyTo)) mail.ReplyToList.Add(mailReplyTo);
@@ -40,19 +46,52 @@ namespace Gabs.Helpers
             mail.IsBodyHtml = IS_HTML;
             mail.Body = body;
 
-            if (attachment != null && string.IsNullOrEmpty(attachmentFile))
+            try
             {
-                MemoryStream attachmentStream = new MemoryStream(attachment.Data);
-                mail.Attachments.Add(new Attachment(attachmentStream, attachment.FileName, attachment.ContentType));
-                attachmentStream.Dispose();
+                if (attachment != null && string.IsNullOrEmpty(attachmentFile))
+                {
+                    using (var attachmentStream = new MemoryStream(attachment.Data))
+                    {
+                        mail.Attachments.Add(new Attachment(attachmentStream, attachment.FileName, attachment.ContentType));
+                        smtp.Send(mail);
+                    }
+                    return true;
+                }
+                else if (!string.IsNullOrEmpty(attachmentFile) && attachment == null)
+                {
+                    mail.Attachments.Add(new Attachment(attachmentFile));
+                }
+
+                smtp.Send(mail);
+                return true;
             }
-            if (!string.IsNullOrEmpty(attachmentFile) && attachment == null)
+            catch
             {
-                mail.Attachments.Add(new Attachment(attachmentFile));
+                return false;
+            }
+        }
+
+        public static bool SendEmail(string mailTo, string subject, string body, EmailAttachment attachment = null, string attachmentFile = "", string mailCc = "", string mailBc = "", string mailReplyTo = "")
+        {
+            return SendEmail(new List<string> { mailTo }, subject, body, attachment, attachmentFile, mailCc, mailBc, mailReplyTo);
+        }
+
+        public static EmailAttachment FileToAttachment(System.Web.HttpPostedFileBase File)
+        {
+            if (File != null)
+            {
+                var attachment = new EmailAttachment
+                {
+                    Data = new byte[File.ContentLength],
+                    FileName = File.FileName,
+                    ContentType = File.ContentType
+                };
+                File.InputStream.Read(attachment.Data, 0, File.ContentLength);
+
+                return attachment;
             }
 
-            smtp.Send(mail);
-            return true;
+            return null;
         }
 
         public class EmailAttachment
